40e12cba65e772cbf9111236f28c9863cc3f5bad
Malypoeur <malypoeur@gmail.com>
2016-10-16 09:57:40 +0700

    Listener Pattern on RecyclerView

---
+++ app/src/main/java/com/example/vanna/attendance2/history/AttendanceAdapterListener.java
@@ -0,0 +1,11 @@
+package com.example.vanna.attendance2.history;
+
+/**
+ * Created by malypoeur on 10/16/16.
+ */
+
+public interface AttendanceAdapterListener {
+
+    public void onStatusClick(int position);
+
+}
--- app/src/main/java/com/example/vanna/attendance2/history/AttenddanceHistoryActivity.java
+++ app/src/main/java/com/example/vanna/attendance2/history/AttenddanceHistoryActivity.java
@@ -4,6 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.example.vanna.attendance2.R;
 
@@ -14,16 +18,49 @@ public class AttenddanceHistoryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_attenddance_history);
 
+        initLayout();
+
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
 
         // setup adapter
         Attendance[] data = getAttendanceData();
-        AttendanceArrayAdapter adapter = new AttendanceArrayAdapter(data);
+        AttendanceArrayAdapter adapter = new AttendanceArrayAdapter(this, data);
+        adapter.setAttendanceAdapterListener(new AttendanceAdapterListener() {
+            @Override
+            public void onStatusClick(int position) {
+                Log.d("AttendanceActivity: ", "on Listener, received position: " + position);
+                visibleLayout(position);
+            }
+        });
         recyclerView.setAdapter(adapter);
 
         // setup layout manager
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
         recyclerView.setLayoutManager(linearLayoutManager);
+    }
+
+    private void initLayout(){
+        TextView buttonClose = (TextView)findViewById(R.id.statusClose);
+        buttonClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                invisibleLayout();
+            }
+        });
+    }
+
+    private void invisibleLayout(){
+        RelativeLayout relativeLayout = (RelativeLayout)findViewById(R.id.statusDetailLayout);
+        relativeLayout.setVisibility(View.INVISIBLE);
+    }
+
+    private void visibleLayout(int position){
+        RelativeLayout relativeLayout = (RelativeLayout)findViewById(R.id.statusDetailLayout);
+        relativeLayout.setVisibility(View.VISIBLE);
+
+        TextView textView = (TextView)findViewById(R.id.statusDetailView);
+        textView.setText("Clicked on position: " + position);
+
     }
 
     private Attendance[] getAttendanceData() {
